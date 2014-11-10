7c78d685723638153679b8cc903cef5248c8cca7
ketian <xuketian@gmail.com>
2014-11-10 16:22:52 +0800

    backup, still a lot to do... T T

---
+++ src/API/API.cpp
@@ -0,0 +1,6 @@
+#include "API.hpp"
+
+/// TODO...
+///
+/// TxT
+///
---
+++ src/API/API.hpp
@@ -0,0 +1,52 @@
+/** API
+ *  Created by Ketian
+ */
+
+#ifndef API_HPP
+#define API_HPP
+
+typedef std::vector < Condition > Conditions;
+
+class Condition {
+private:
+    std::string key;
+    Operator op;
+    void *valuePtr;
+
+public:
+    void setKey(const std::string &key);
+    void setOp(const Operator &op);
+    void setValue(const void *valuePtr);
+
+    std::string getKey();
+    Operator getOp();
+    void *getValuePtr();
+};
+
+
+class API {
+private:
+    RecordManager &recordManager;
+    IndexManager &indexManager;
+    CatalogManger &catalogManager;
+
+public:
+    API(const CatalogManager &cat,
+        const RecordManager &rec,
+        const IndexManager &ind
+        ):catalogManager(cat), recordManager(rec), indexManager(ind) {}
+
+    ~API() {}
+
+    void createTable(const std::string &tableName, const std::vector < Scheme > &data);
+
+    void dropTable(const std::string &tableName);   // indexManager.dropAddress() should be called
+    void dropIndex(const std::string &tableName);
+    void select(const std::string &tableName,
+                const Conditions &conditions);      // no "or" in statement
+    ConstItemIterator itemsBegin();
+    ConstItemIterator itemsEnd();
+
+};
+
+#endif
