efc9232ecab732b1a6a2d5614c057a9a84d56b5f
Tao J <taoj@outlook.com>
2014-11-16 21:31:26 +0800

    improve: dMNb main() logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <string>
+#include <cstdio>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+
+    char dmnb[] =
+        "       _   __  __   _  _    _      \n"
+        "    __| | |  \\/  | | \\| |  | |__   \n"
+        "   / _` | | |\\/| | | .` |  | '_ \\  \n"
+        "   \\__,_| |_|__|_| |_|\\_|  |_.__/  \n"
+        "  _|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n"
+        "  \"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n"
+        ;
+
+    char sepr[] =
+        " ---------------------------------\n"
+        ;
+
+    char welcm[] = 
+        " Welcome to DminiB system(exclaimation), enter command below\n"
+        ;
+
+    cout << dmnb << sepr << welcm << endl;
+
+    string str;
+    do {
+
+        str.clear();
+        cout << "dMNb>";
+        cin >> str;
+        cout << str << endl;
+
+    } while (str.length()); 
+
     return 0;
 }
 
