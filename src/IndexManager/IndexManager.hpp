1f1bf603787f62bd78440589f456b48db848feff
Xin Hao <xinhao20122013@gmail.com>
2014-11-17 20:50:21 +0800

    back up indexmanager

--- src/IndexManager/IndexManager.hpp
+++ src/IndexManager/IndexManager.hpp
@@ -1,28 +1,29 @@
 #ifndef IndexManager_HPP
 #define IndexManager_HPP
-
+#include ""
 #include <list>
 #include <map>
 #include <set>
+#include <string>
 #include "BtreeIndex.hpp"
 
 class IndexManager{
 private:
 	int indexcount;
-//	std::set tid->attrid;
-    std::set<int> indexid;
-//	std::map<tid,attrid>->filePoint;
+    std::map<std::string,std::string> indexList;//表+属性--索引名
+    std::map<std::string,int> rootList;//表名根节点地址
+    std::map<std::string,int> headList;//表名头节点地址
+    std::map<std::string,int> dataLength;//每个表一个节点的长度
+    std::map<std::string,int> dataTypeList;//每个表key种类
 	BtreeIndex *Btree;
 	
 public:
-	bool hasIndex(int tid,int attrid);
-	bool isIndex(int tid,int indexid);
-	int drop_index(int indexid);
-	int create_index(int tid,int attrid,int indexid);
-// FIXME: void create_index(const std::string tableName,
-//                          const std::string colName,
-//                          const std::string indexName,
-//                          DataType type);
+    bool hasIndex(const std::string tableName,const std::string attrName);
+    int drop_index(const std::string indexName);
+    void create_index(const std::string tableName,
+                      const std::string colName,
+                      const std::string indexName,
+                      DataType type);
 // 在 tableName 的 colName 上建立索引 indexName, 这个 colName 的类型是 type
 	int search(int tid,int attrid,int key,int condition_info);
 	int search(int tid,int attrid,float key,int condition_info);
