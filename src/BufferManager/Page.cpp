ff8aace6a99c3ec1facb1bd98a8a1db58f9697b4
dodoleon <gaotao@gaotaodeMacBook-Air.local>
2014-11-17 16:29:59 +0800

    改了一点错误

--- src/BufferManager/Page.cpp
+++ src/BufferManager/Page.cpp
@@ -4,28 +4,20 @@ using namespace std;
 void Page::read_from_file()
 {
 	ifstream infile;
-	char *name;
-	name = new char[filename.length()];
-	strcpy(name, filename.c_str());
-	infile.open(name, ios::binary | ios::in);
+	infile.open(filename.c_str(), ios::binary | ios::in);
 	infile.seekg(pages_addr, ios::beg);
 	infile.read(data, 4096);
 	call_time = 0;
-	delete[] name;
 	infile.close();
 }
 
 void Page::write_to_file()
 {
 	ofstream outfile;
-	char *name;
-	name = new char[filename.length()];
-	strcpy(name, filename.c_str());
-	outfile.open(name, ios::binary | ios::out | ios::app);
+	outfile.open(filename.c_str(), ios::binary | ios::out | ios::app);
 	outfile.seekp(pages_addr, ios::beg);
 	outfile.write(data, 4096);
 	call_time = -1;
-	delete[] name;
 	outfile.close();
 }
 
