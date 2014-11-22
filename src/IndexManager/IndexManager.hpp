854f1a3d3530fb4a26cca5091a868245a57873aa
Xin Hao <xinhao20122013@gmail.com>
2014-11-22 22:58:34 +0800

    finish all

--- src/IndexManager/IndexManager.hpp
+++ src/IndexManager/IndexManager.hpp
@@ -9,15 +9,19 @@
 #include "Utility.hpp"
 #include "BtreeIndex.hpp"
 using namespace utls;
+using namespace std;
 class IndexManager{
 typedef std::map<std::string,unsigned int> NodeList;
-private:
-    std::map<std::string,std::string> indexList;//表+属性--索引名
-    std::map<std::string,std::string> tableList;
+typedef std::map<std::string,std::string> StringList;
+typedef std::map<std::string,DataType> TypeList;
+public:
+    StringList indexList;//表+属性--索引名
+    StringList tableList;
     NodeList rootList;//表名根节点地址
     NodeList headList;//表名头节点地址
-    std::map<std::string,DataType> dataTypeList;//每个表key种类
+    TypeList dataTypeList;//每个表key种类
     BufferManager &buffer;
+    std::list<filepoint> ans;
     //    std::map<std::string,int> dataLength;//每个表一个节点的长度
 	
 public:
@@ -25,16 +29,18 @@ public:
         std::ifstream ifile("IndexManager.txt");
         std::string indexname,tablename;
         DataType dtype;
+        int temptype;
         unsigned int root,first;
         if (ifile){
             while(!ifile.eof()){
-                ifile>>indexname>>tablename>>root>>first>>dtype;
+                ifile>>indexname>>tablename>>root>>first>>temptype;
                 indexList[tablename] = indexname;
                 tableList[indexname] = tablename;
                 rootList[indexname] = root;
                 headList[indexname] = first;
-                dataTypeList[indexname] = dtype;
+                dataTypeList[indexname] = DataType(temptype);
             }
+            ifile.close ();
         }else{
             indexList.clear ();
             tableList.clear ();
@@ -43,7 +49,9 @@ public:
             dataTypeList.clear ();
         }
     }
-    ~IndexManager(){
+    ~IndexManager(){}
+
+    void save(){
         std::ofstream ofile("IndexManager.txt");
         std::string indexname;
         NodeList::const_iterator  it=rootList.begin();
@@ -54,6 +62,51 @@ public:
                    " "<<headList[indexname]<<" "<<
                    " "<<dataTypeList[indexname]<<" "<<std::endl;
         }
+        ofile.close ();
+    }
+
+    void print(){
+        cout<<"_______indexList_____"<<endl;
+        StringList::iterator it0;
+        for (it0 = indexList.begin ();it0!=indexList.end ();it0++){
+            cout<<it0->first<<"->"<<it0->second<<endl;
+        }
+        cout<<"_______tableList_____"<<endl;
+        StringList::iterator it1;
+        for (it1 = tableList.begin ();it1!=tableList.end ();it1++){
+            cout<<it1->first<<"->"<<it1->second<<endl;
+        }
+        cout<<"_______rootList_____"<<endl;
+        NodeList::iterator it2;
+        for (it2 = rootList.begin ();it2!=rootList.end ();it2++){
+            cout<<it2->first<<"->"<<it2->second<<endl;
+        }
+        cout<<"_______headList_____"<<endl;
+        NodeList::iterator it3;
+        for (it3 = headList.begin ();it3!=headList.end ();it3++){
+            cout<<it3->first<<"->"<<it3->second<<endl;
+        }
+        cout<<"_______dataTypeList_____"<<endl;
+        TypeList::iterator it4;
+        for (it4 = dataTypeList.begin ();it4!=dataTypeList.end ();it4++){
+            cout<<it4->first<<"->"<<it4->second<<endl;
+        }
+    }
+    void clear(){
+        cout<<"clear()"<<endl;
+        StringList::iterator it0;
+        for (it0 = indexList.begin ();it0!=indexList.end ();it0++){
+            struct DataAddr fp;
+            fp.filename = it0->second;
+            fp.datalen = 128;
+            buffer.drop (fp);
+        }
+        system("del IndexManager.txt");
+        indexList.clear ();
+        tableList.clear ();
+        rootList.clear ();
+        headList.clear ();
+        dataTypeList.clear ();
     }
 
     bool hasIndex(const std::string tableName,const std::string colName){
@@ -81,25 +134,36 @@ public:
         filepoint root = rootList[indexName];
         filepoint first = headList[indexName];
         switch (dtype){
-            case INT:
+            case INT:{
                 BtreeIndex<int> btree0(buffer,indexName,root,first);
                 btree0.drop ();
+            }
                 break;
-            case FLOAT:
+            case FLOAT:{
                 BtreeIndex<float> btree1(buffer,indexName,root,first);
                 btree1.drop ();
                 break;
+            }
         }
-        tableList.erase (tableList.find (indexName),tableList.find (indexName));
-        rootList.erase (rootList.find (indexName),rootList.find (indexName));
-        headList.erase (headList.find (indexName),headList.find (indexName));
-        dataTypeList.erase (dataTypeList.find (indexName),dataTypeList.find (indexName));
+        indexList.erase (indexList.find(tableList[indexName]));
+        tableList.erase (tableList.find (indexName));
+        rootList.erase (rootList.find (indexName));
+        headList.erase (headList.find (indexName));
+        dataTypeList.erase (dataTypeList.find (indexName));
+        struct DataAddr fp;
+        fp.filename = indexName;
+        fp.datalen = 128;
+        buffer.drop (fp);
     }
 
     void createIndex(const std::string tableName,
                       const std::string colName,
                       const std::string indexName,
                       DataType type){
+        struct DataAddr fp;
+        fp.filename = indexName;
+        fp.datalen = 128;
+        buffer.create (fp);
         indexList[tableName+"_"+colName] = indexName;
         dataTypeList[indexName] = type;
         rootList[indexName] = 0;
@@ -114,99 +178,143 @@ public:
         filepoint root = rootList[indexname];
         filepoint first = headList[indexname];
         switch (dtype){
-            case INT:
+            case INT:{
                 int key0 = *((int*)dataPtr);
                 BtreeIndex<int> btree0(buffer,indexname,root,first);
+//                cout<<"BtreeIndex"<<key0<<pos<<" "<<root<<" "<<first<<endl;
                 btree0.insert (key0,pos);
+//                btree0.print ();
                 rootList[indexname] = btree0.getRoot ();
                 headList[indexname] = btree0.getFirst ();
+            }
                 break;
-            case FLOAT:
+            case FLOAT:{
                 float key1 = *((float*)dataPtr);
                 BtreeIndex<float> btree1(buffer,indexname,root,first);
                 btree1.insert (key1,pos);
                 rootList[indexname] = btree1.getRoot ();
                 headList[indexname] = btree1.getFirst ();
+            }
                 break;
         }
     }
 
 // 在 tableName 的 colName 上建立索引 indexName, 这个 colName 的类型是 type
-    std::list <unsigned int> select(const std::string &indexname,const Condition &condition_info){
-        DataType dtype = dataTypeList[indexname];
-        filepoint root = rootList[indexname];
-        filepoint first = headList[indexname];
+    std::list <unsigned int> &select(const std::string &indexName,const Condition &condition_info){
+        ans.clear ();
+        DataType dtype = dataTypeList[indexName];
+        filepoint root = rootList[indexName];
+        filepoint first = headList[indexName];
         switch (dtype){
-            case INT:
-                BtreeIndex<int> btree0(buffer,indexname,root,first);
+            case INT:{
+                BtreeIndex<int> btree0(buffer,indexName,root,first);
                 int key0 = condition_info.intv;
-
+//                btree0.print ();
+//                std::cout<<"key0 "<<key0<<std::endl;
                 switch (condition_info.op) {
-                case EQUAL:
-                        return btree0.findeq (key0);
+                case EQUAL:{
+                    ans = btree0.findeq (key0);
+                    return ans;
+                }
                     break;
-                case LESS:
-                        return btree0.findless (key0);
+                case LESS:{
+                    ans = btree0.findless (key0);
+                    return ans;
+                }
                     break;
-                case NO_MORE:
-                        return btree0.findlesseq (key0);
+                case NO_MORE:{
+                    ans = btree0.findlesseq (key0);
+                    return ans;
+                }
                     break;
-                case MORE:
-                        return btree0.findbig (key0);
+                case MORE:{
+                    ans = btree0.findbig (key0);
+                    return ans;
+                }
                     break;
-                case NO_LESS:
-                        return btree0.findbigeq (key0);
+                case NO_LESS:{
+                    ans = btree0.findbigeq (key0);
+                    return ans;
+                }
                     break;
-                case UNEQUAL:
-                        return btree0.findbig (key0).insert (btree0.findbig (key0).end (),btree0.findless (key0).begin (),btree0.findbig (key0).end ());
+                case UNEQUAL:{
+                    std::list<filepoint> ans1;
+                    ans = btree0.findbig (key0);
+                    ans1 = btree0.findless (key0);
+                    ans.merge (ans1);
+                    return ans;
+                }
                     break;
-                default:
+                default:{}
                     break;
                 }
+        }
                 break;
-            case FLOAT:
-                BtreeIndex<float> btree1(buffer,root,first);
+            case FLOAT:{
+                BtreeIndex<float> btree1(buffer,indexName,root,first);
                 int key1 = condition_info.floatv;
                 switch (condition_info.op) {
-                case EQUAL:
-                        return btree1.findeq (key1);
+                case EQUAL:{
+                    ans = btree1.findeq (key1);
+                    return ans;
+                }
                     break;
-                case LESS:
-                        return btree1.findless (key1);
+                case LESS:{
+                    ans = btree1.findless (key1);
+                    return ans;
+                }
                     break;
-                case NO_MORE:
-                        return btree1.findlesseq (key1);
+                case NO_MORE:{
+                    ans = btree1.findlesseq (key1);
+                    return ans;
+                }
                     break;
-                case MORE:
-                        return btree1.findbig (key1);
+                case MORE:{
+                    ans = btree1.findbig (key1);
+                    return ans;
+                }
                     break;
-                case NO_LESS:
-                        return btree1.findbigeq (key1);
+                case NO_LESS:{
+                    ans = btree1.findbigeq (key1);
+                    return ans;
+                }
                     break;
-                case UNEQUAL:
-                        return btree1.findbig (key1).insert (btree1.findbig (key1).end (),btree1.findless (key1).begin (),btree1.findbig (key1).end ());
+                case UNEQUAL:{
+                    std::list<filepoint> ans1;
+                    ans = btree1.findbig (key1);
+                    ans1 = btree1.findless (key1);
+                    ans.merge (ans1);
+                    return ans;
+                }
                     break;
-                default:
+                default:{}
                     break;
                 }
+        }
                 break;
         }
    }
 
-    void remove(const std::string &indexname,void *dataPtr){
-        DataType dtype = dataTypeList[indexname];
-        filepoint root = rootList[indexname];
-        filepoint first = headList[indexname];
+    void remove(const std::string &indexName,void *dataPtr){
+        DataType dtype = dataTypeList[indexName];
+        filepoint root = rootList[indexName];
+        filepoint first = headList[indexName];
         switch (dtype){
-            case INT:
+            case INT:{
                 int key0 = *((int*)dataPtr);
-                BtreeIndex<int> btree0(buffer,root,first);
+                BtreeIndex<int> btree0(buffer,indexName,root,first);
                 btree0.remove (key0);
+                rootList[indexName] = btree0.getRoot ();
+                headList[indexName] = btree0.getFirst ();
+            }
                 break;
-            case FLOAT:
+            case FLOAT:{
                 float key1 = *((float*)dataPtr);
-                BtreeIndex<float> btree1(buffer,root,first);
+                BtreeIndex<float> btree1(buffer,indexName,root,first);
                 btree1.remove (key1);
+                rootList[indexName] = btree1.getRoot ();
+                headList[indexName] = btree1.getFirst ();
+            }
                 break;
         }
     }
@@ -214,37 +322,31 @@ public:
     void remove(const std::string indexName){
         if (tableList.count (indexName) == 0){
             std::cout<<"no index exit to drop!"<<std::endl;
-            return false;
+            return;
         }
-        DataType dtype = dataTypeList[indexname];
-        filepoint root = rootList[indexname];
-        filepoint first = headList[indexname];
-        switch (dtype){
-            case INT:
-                BtreeIndex<int> btree0(buffer,indexname,root,first);
-                btree0.clear ();
-                break;
-            case FLOAT:
-                BtreeIndex<float> btree1(buffer,indexname,root,first);
-                btree1.clear ();
-                break;
-        }
-        rootList[indexname] = 0;
-        headList[indexname] = 0;
+        struct DataAddr fp;
+        fp.filename = indexName;
+        fp.datalen = 128;
+        buffer.drop (fp);
+        buffer.create (fp);
+
+//        switch (dtype){
+//            case INT:{
+//                BtreeIndex<int> btree0(buffer,indexName,root,first);
+//                btree0.clear ();
+//            }
+//                break;
+//            case FLOAT:{
+//                BtreeIndex<float> btree1(buffer,indexName,root,first);
+//                btree1.clear ();
+//            }
+//                break;
+//            case TOTAL_TYPE:{
+//            }
+//                break;
+//        }
+        rootList[indexName] = 0;
+        headList[indexName] = 0;
     }
-
-
-
-// FIXME: void insert(const std::string indexName,
-//                    const int &pos,
-//                    void *dataPtr);
-// 在 indexName 中插入一个点，对应硬盘上的位置 pos，具体数据存在 dataPtr，
-// 根据 indexName 可以判断出这个数据是什么类型的（建立索引的时候可以记录一下）
-
-//    int delete(int tid,int eid,int attrid,std::vector<char> v);
-// FIXME: `delete` is a reversed word in C++, change a name plz...
-
-// TODO:
-// 返回表 tableName 建立在 colName 上的索引名，没有建立索引返回空串
 };
 #endif
