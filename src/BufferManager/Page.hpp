679cdd7854f8f364b46b159903865e17c4121caf
dodoleon <gaotao@gaotaodeMacBook-Air.local>
2014-11-22 14:48:38 +0800

    make buffer faster

--- src/BufferManager/Page.hpp
+++ src/BufferManager/Page.hpp
@@ -1,11 +1,7 @@
 ﻿#ifndef PAGE_H
 #define PAGE_H
 #include <string>
-struct pagedata{
-	unsigned offset;
-	unsigned datalen;
-	char *data;
-};  
+
 class Page{
 public:
 	char data[4096];//page 的4K数据
@@ -15,8 +11,8 @@ public:
 
 	void write_to_file();
 	void read_from_file();
-	void write_page(const pagedata info);//info.date的长度必须与info.len相等
-	void read_page(pagedata & info); //info.date事先开好空间。
+	//void write_page(const pagedata info);//info.date的长度必须与info.len相等
+	//void read_page(pagedata & info); //info.date事先开好空间。
 	Page()
 	{
 		call_time = -1;
